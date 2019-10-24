ba9f40c522ecb48e9c388da2fdbc5a9f854eadaf
Dylan Ellison <dylancellison@gmail.com>
2019-10-24 00:45:52 -0500

    serve static files in prod

--- src/index.js
+++ src/index.js
@@ -22,4 +22,15 @@ app.use(bodyParser.json({ type: '*/*' }));
 
 router(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve production assets if they exist
+  app.use(express.static('client/build'));
+
+  // Express will serve index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(5000, () => console.log('Listening on port 5000'));
